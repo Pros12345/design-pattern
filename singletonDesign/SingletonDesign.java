2c7cad4cc288b8e6169d2ccc35804456c6727c9a
Prosenjit <86827153+Pros12345@users.noreply.github.com>
2026-02-05 14:51:08 +0530

    Add files via upload

--- singletonDesign/SingletonDesign.java
+++ singletonDesign/SingletonDesign.java
@@ -1,1 +1,21 @@
+package SingletonDesign;
 
+public class SingletonDesign {
+	public static void main(String[] args) {
+		SingleTonDesignObject sd = SingleTonDesignObject.getInstance();
+	}
+}
+
+class SingleTonDesignObject {
+	private static SingleTonDesignObject s;
+
+	private SingleTonDesignObject() {
+
+	}
+
+	public static synchronized SingleTonDesignObject getInstance() {
+		if (s == null)
+			s = new SingleTonDesignObject();
+		return s;
+	}
+}
